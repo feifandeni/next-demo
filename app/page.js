a7c17960c62620f6c3d1ef8d0252a28022651963
ning.zhang <ning.zhang2571@xquant.com>
2023-06-27 17:57:23 +0800

    提交代码

--- app/page.js
+++ app/page.js
@@ -8,7 +8,6 @@ export default function Home() {
         测试网址
         <div>
           <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
             target="_blank"
             rel="noopener noreferrer"
           >
